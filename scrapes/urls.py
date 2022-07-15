d90ca8dcdb683a325f6ebae4147da35c3a9ca8b4
Juanje Marquez <juanje.mn@gmail.com>
2022-07-15 19:39:09 +0200

    Added download csv to scrapejobs

--- scrapes/urls.py
+++ scrapes/urls.py
@@ -1,9 +1,11 @@
 # scrapes/urls.py
+from django import views
 from django.urls import path
 
-from .views import ScrapesListView, ScrapesDetailView
+from .views import ScrapesListView, ScrapesDetailView, scrapejob_csv
 
 urlpatterns = [
     path("", ScrapesListView.as_view(), name="scrapes_list"),
     path("<uuid:pk>/", ScrapesDetailView.as_view(), name="scrapes_detail"),
+    path("csv/<origin>/<uuid:pk>/", scrapejob_csv, name="scrapejob_csv"),
 ]
--- scrapes/views.py
+++ scrapes/views.py
@@ -1,7 +1,17 @@
 # scrapes/views.py
+from datetime import date
+from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 
-from .models import ScrapeJob
+from .models import (
+    PlayerCapology,
+    PlayerFbref,
+    PlayerFbrefGK,
+    PlayerTransfermarkt,
+    PlayerUnderstat,
+    ScrapeJob,
+)
+import csv
 
 
 class ScrapesListView(ListView):
@@ -14,3 +24,51 @@ class ScrapesDetailView(DetailView):
     model = ScrapeJob
     context_object_name = "scrapejob"
     template_name = "scrapes/scrapejob_detail.html"
+
+
+def scrapejob_csv(request, pk, origin):
+    today = date.today().strftime("%Y%m%d")
+    filename = "{}_{}_data.csv".format(today, origin)
+
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = "attachment; filename={}".format(filename)
+
+    # Creamos el writer del CSV
+    writer = csv.writer(response)
+
+    # Diseñamos el modelo
+    if origin == "FB":
+        mod = "PlayerFbref"
+        data = PlayerFbref.objects.filter(scrape_job=pk).values_list()
+        colums = PlayerFbref._meta.get_fields()
+    if origin == "FG":
+        mod = "PlayerFbrefGK"
+        data = PlayerFbrefGK.objects.filter(scrape_job=pk).values_list()
+        colums = PlayerFbrefGK._meta.get_fields()
+    if origin == "CA":
+        mod = "PlayerCapology"
+        data = PlayerCapology.objects.filter(scrape_job=pk).values_list()
+        colums = PlayerCapology._meta.get_fields()
+    if origin == "TM":
+        mod = "PlayerTransfermarkt"
+        data = PlayerTransfermarkt.objects.filter(scrape_job=pk).values_list()
+        colums = PlayerTransfermarkt._meta.get_fields()
+    if origin == "US":
+        mod = "PlayerUnderstat"
+        data = PlayerUnderstat.objects.filter(scrape_job=pk).values_list()
+        colums = PlayerUnderstat._meta.get_fields()
+
+    # Limpiamos las columnas
+    cols = []
+    replace_text = "scrapes.{}.".format(mod)
+    for col in colums:
+        cols.append(str(col).replace(replace_text, ""))
+
+    # Añadimos el header limpio
+    writer.writerow(cols)
+
+    # Ciclo para recorrer las filas
+    for row in data:
+        writer.writerow(row)
+
+    return response
