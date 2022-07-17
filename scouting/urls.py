b4b517d32cbe740375af86393cce61e0c8536204
Juanje Marquez <juanje.mn@gmail.com>
2022-07-17 11:43:23 +0200

    Completadas vistas de control de reportes de rendimiento de jugadores en su app

--- scouting/urls.py
+++ scouting/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import CreateReport
+from .views import CreateReport, UpdateReport, ReportList, DeleteReport
 
 
 urlpatterns = [
-    path("create/<uuid:pk>/", CreateReport.as_view(), name="creation_report")
+    path("", ReportList.as_view(), name="reports_list_view"),
+    path("<uuid:pk>/create_report/", CreateReport.as_view(), name="creation_report"),
+    path("<uuid:pk>/update_report/", UpdateReport.as_view(), name="update_report_view"),
+    path("<uuid:pk>/delete_report/", DeleteReport.as_view(), name="delete_report_view"),
 ]
--- scouting/views.py
+++ scouting/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
 
 from players.models import Player
 
@@ -18,7 +18,7 @@ from .forms import (
 
 
 class CreateReport(CreateView):
-    Model = PerformanceReport
+    model = PerformanceReport
     template_name = "scouting/create_report.html"
 
     def get_context_data(self, **kwargs):
@@ -58,4 +58,58 @@ class CreateReport(CreateView):
             return PerformanceReportForm
 
     def get_success_url(self):
-        return reverse("players_list")
+        return reverse("reports_list_view")
+
+
+class ReportList(ListView):
+    model = PerformanceReport
+    context_object_name = "reports_list"
+    template_name = "scouting/reports_list.html"
+
+
+class UpdateReport(UpdateView):
+    model = PerformanceReport
+    template_name = "scouting/update_report.html"
+
+    def get_queryset(self, **kwargs):
+        return super().get_queryset()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["report_pk"] = self.kwargs["pk"]
+        context["player"] = Player.objects.get(
+            pk=PerformanceReport.objects.get(pk=self.kwargs["pk"]).player.pk
+        )
+        return context
+
+    def get_form_class(self):
+        player_x = Player.objects.get(
+            pk=PerformanceReport.objects.get(pk=self.kwargs["pk"]).player.pk
+        )
+        if player_x.position_norm == "Portero":
+            return PerformanceReportPorteroForm
+        elif player_x.position_norm == "Central":
+            return PerformanceReportCentralForm
+        elif player_x.position_norm == "Lateral":
+            return PerformanceReportLateralForm
+        elif player_x.position_norm == "Mediocentro":
+            return PerformanceReportMediocentroForm
+        elif player_x.position_norm == "Medio Ofensivo":
+            return PerformanceReportMedioOfensivoForm
+        elif player_x.position_norm == "Extremo":
+            return PerformanceReportExtremoForm
+        elif player_x.position_norm == "Delantero":
+            return PerformanceReportDelanteroForm
+        else:
+            return PerformanceReportForm
+
+    def get_success_url(self):
+        return reverse("reports_list_view")
+
+
+class DeleteReport(DeleteView):
+    model = PerformanceReport
+    template_name = "scouting/report_delete_confirm.html"
+
+    def get_success_url(self):
+        return reverse("reports_list_view")
